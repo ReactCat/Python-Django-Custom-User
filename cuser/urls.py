76375b8a19ff0c3b722e742c1318fb58cc941738
ReactCat <kbearconsulting@gmail.com>
2022-08-28 10:51:59 +0900

    Custom User Code

---
+++ cuser/urls.py
@@ -0,0 +1,11 @@
+"""ecom Main URL Configuration """
+
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home')
+
+]
